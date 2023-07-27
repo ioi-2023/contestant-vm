c0c3c34aa7cada374b4b8a0e5a31ab761fa8d697
Barnabás Pőcze <pobrn@protonmail.com>
2023-07-27 16:08:14 +0200

    Do not install openssh-server in setup.sh
    The automatic installation already takes care of installing `openssh-server`,
    so don't do it in `setup.sh` as it is redundant.

--- setup.sh
+++ setup.sh
@@ -59,7 +59,7 @@ netplan apply
 
 # Install tools needed for management and monitoring
 
-apt -y install net-tools openssh-server ansible xvfb tinc oathtool imagemagick \
+apt -y install net-tools ansible xvfb tinc oathtool imagemagick \
 	zabbix-agent
 
 # Install local build tools
